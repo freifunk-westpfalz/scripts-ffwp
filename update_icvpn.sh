e00e50395f7511578703c8c2aa5873107d4af556
paalsteek <github@paalsteek.de>
2015-03-21 23:19:43 +0100

    run gentinccfg from update_icvpn.sh

--- update_icvpn.sh
+++ update_icvpn.sh
@@ -4,7 +4,8 @@ PEERSDIR="/etc/tinc/icvpn/"
 BIRDDIR="/etc/bird/"
 CONFDIR="/home/freifunk/config/"
 METADIR="/home/freifunk/icvpn/meta/"
-SCRIPTDIR="/home/freifunk/icvpn/scripts/"
+ICVPNSCRIPTDIR="/home/freifunk/icvpn/scripts/"
+SCRIPTDIR="/home/freifunk/scripts/"
 
 # update tinc peers
 cd "${PEERSDIR}"
@@ -14,10 +15,12 @@ git pull
 cd "${METADIR}"
 git pull
 
+"${SCRIPTDIR}"/gentinccfg "$1"
+
 # generate bgp config
 # pwd should be meta repo
-"${SCRIPTDIR}"/mkbgp -f bird -4 -s . -d bgp_icvpn -x westpfalz > "${BIRDDIR}"/bird_bgp.conf
-"${SCRIPTDIR}"/mkbgp -f bird -6 -s . -d bgp_icvpn -x westpfalz > "${BIRDDIR}"/bird6_bgp.conf
+"${ICVPNSCRIPTDIR}"/mkbgp -f bird -4 -s . -d bgp_icvpn -x westpfalz > "${BIRDDIR}"/bird_bgp.conf
+"${ICVPNSCRIPTDIR}"/mkbgp -f bird -6 -s . -d bgp_icvpn -x westpfalz > "${BIRDDIR}"/bird6_bgp.conf
 
 cat "${CONFDIR}"/bird_head.conf "${BIRDDIR}"/bird_bgp.conf > "${BIRDDIR}"/bird.conf
 cat "${CONFDIR}"/bird6_head.conf "${BIRDDIR}"/bird6_bgp.conf > "${BIRDDIR}"/bird6.conf
@@ -27,5 +30,5 @@ birdc6 configure
 
 # generate dns config
 # pwd should be meta repo
-"${SCRIPTDIR}"/mkdns -f dnsmasq -s . -x westpfalz > /etc/dnsmasq.d/icvpn.conf
+"${ICVPNSCRIPTDIR}"/mkdns -f dnsmasq -s . -x westpfalz > /etc/dnsmasq.d/icvpn.conf
 /etc/init.d/dnsmasq restart
